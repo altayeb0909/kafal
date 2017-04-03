20a05169be5024a4a712debafc423b6d6cc511a5
Shubham Pandey <shubham9411@gmail.com>
2017-04-04 00:23:37 +0530

    Theme name and removel of clean blog js

--- functions.php
+++ functions.php
@@ -117,8 +117,6 @@ function kafal_scripts() {
 
 	wp_enqueue_script( 'kafal-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1.0', true );
 	
-	wp_enqueue_script( 'kafal-clean-blog-js', get_template_directory_uri() . '/js/clean-blog.min.js', array(), '20151215', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
